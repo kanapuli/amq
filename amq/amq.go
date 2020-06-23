3afb7759ea8c22f0116313d59fc1a99587d51a2a
kanapuli <athavankanapuli@gmail.com>
2020-06-23 06:47:46 +0530

    Fix flag names

--- amq/amq.go
+++ amq/amq.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	config := parseCliArgs()
-	fmt.Println(config)
+	fmt.Println(*config.Port, *config.ProfilingEnabled, *config.PromMetrics)
 }
 
 //Config is the configuration of amq
@@ -20,8 +20,8 @@ type Config struct {
 func parseCliArgs() Config {
 	conf := Config{}
 	conf.Port = flag.Int("port", 3737, "Port number for amq to run")
-	conf.ProfilingEnabled = flag.Bool("profile", false, "Flag to enable pprof profiling")
-	conf.PromMetrics = flag.Bool("collectmetics", false, "Flag to serve metrics endpoint for prometheus")
+	conf.ProfilingEnabled = flag.Bool("pprof", false, "Flag to enable pprof profiling")
+	conf.PromMetrics = flag.Bool("metrics", false, "Flag to serve metrics endpoint for prometheus")
 	flag.Parse()
 	return conf
 }
